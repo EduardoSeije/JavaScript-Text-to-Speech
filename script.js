30e1ec7ff1daf3e8637e07c279f727cf502128fb
Eduardo Seije <eduardoseije@gmail.com>
2021-07-27 10:58:19 -0300

    Add event listener to the play button. Create the playText function that uses the speechSynthesis API, and disable the text area when the play button is clicked

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const playBtn = document.getElementById('play-button');
 const pauseBtn = document.getElementById('pause-button');
 const stopBtn = document.getElementById('stop-button');
+const textInput = document.getElementById('text');
+const speedInput = document.getElementById('speed');
+
+playBtn.addEventListener('click', () => {
+  playText(textInput.value);
+})
+
+function playText(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.rate = speedInput.value || 1
+  utterance.addEventListener('end', () => {
+    textInput.disabled = false;
+  })
+  textInput.disabled = true;
+  speechSynthesis.speak(utterance);
+}
