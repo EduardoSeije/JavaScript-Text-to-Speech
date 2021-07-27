ec508b1af3a7a54042f2991e1dda5f7dfb3a27ae
Eduardo Seije <eduardoseije@gmail.com>
2021-07-27 11:04:34 -0300

    Add event listener to the pause button and a condition to the playText funtion to resume the speech

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ const speedInput = document.getElementById('speed');
 
 playBtn.addEventListener('click', () => {
   playText(textInput.value);
-})
+});
+
+pauseBtn.addEventListener('click', pauseText);
 
 function playText(text) {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  };
   const utterance = new SpeechSynthesisUtterance(text);
   utterance.rate = speedInput.value || 1
   utterance.addEventListener('end', () => {
     textInput.disabled = false;
-  })
+  });
   textInput.disabled = true;
   speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+  if (speechSynthesis.speaking) {
+    speechSynthesis.pause();
+  };
 }
